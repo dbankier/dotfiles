88abaa2a67ddb4ec18ffc23b278c0cfc3be34fcf
David Bankier <dbankier@hotmail.com>
2013-08-29 14:57:45 +1000

    vim needs to be symlinked

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig tmux.conf vimrc zshrc"    # list of files/folders to symlink in homedir
+files="gitconfig tmux.conf vim vimrc zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
